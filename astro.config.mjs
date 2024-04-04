1c0367231006498e93bf0d1a6d7f7cecf98f8e89
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-04-04 11:25:37 -0500

    Update astro.config.mjs to include @astrojs/db integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import {defineConfig} from "astro/config";
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind({applyBaseStyles: false})],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  }), db()]
 });
