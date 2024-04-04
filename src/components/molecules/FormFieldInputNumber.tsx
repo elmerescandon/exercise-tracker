76431f87aa9d66700f228e79e63e143c78612ba9
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-04-03 22:08:08 -0500

    Add FormFieldInputNumber component and remove FormInput component

---
+++ src/components/molecules/FormFieldInputNumber.tsx
@@ -0,0 +1,38 @@
+import {FormControl, FormField, FormItem, FormMessage} from "../ui/form";
+import {Input} from "../ui/input";
+
+type FormFieldInputProps = {
+    form: any;
+    name: string;
+    placeholder: string;
+    type?: string;
+};
+
+const FormFieldInput = ({
+    form,
+    name,
+    placeholder,
+    type = "text",
+}: FormFieldInputProps) => {
+    return (
+        <FormField
+            control={form.control}
+            name={name}
+            render={({field}) => (
+                <FormItem>
+                    <FormControl>
+                        <Input
+                            type={type}
+                            placeholder={placeholder}
+                            {...field}
+                        />
+                    </FormControl>
+
+                    <FormMessage />
+                </FormItem>
+            )}
+        />
+    );
+};
+
+export default FormFieldInput;
