78239bf3291543d32c507ec0ab65c7075dc73b66
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-04-03 21:57:01 -0500

    Add users constant array

---
+++ src/lib/constants.ts
@@ -0,0 +1,8 @@
+import type {ISelectOption} from "./interfaces";
+
+export const users: ISelectOption[] = [
+    {label: "Raúl", value: "raul"},
+    {label: "Guti", value: "guti"},
+    {label: "Mapu", value: "mapu"},
+    {label: "Math", value: "mathi"},
+];
