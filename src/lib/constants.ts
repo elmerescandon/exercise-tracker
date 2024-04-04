5e889ec6403f0544c40ca837abc848325f72e19c
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-04-03 22:32:28 -0500

    Add formErrors and inputNumberZ constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,5 @@
 import type {ISelectOption} from "./interfaces";
+import {z} from "zod";
 
 export const users: ISelectOption[] = [
     {label: "Raúl", value: "raul"},
@@ -6,3 +7,17 @@ export const users: ISelectOption[] = [
     {label: "Mapu", value: "mapu"},
     {label: "Math", value: "mathi"},
 ];
+
+export const formErrors = {
+    user: " El usuario es requerido",
+    metric: "La métrica es requerida",
+};
+
+export const inputNumberZ = z.coerce
+    .number({
+        required_error: formErrors.metric,
+    })
+    .refine((value) => value !== 0, {
+        message: formErrors.metric,
+    })
+    .default(0);
