6565f1fbdcc24816b2721378e4208f819cea455f
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-04-03 22:32:25 -0500

    Add input fields and submit button to FormExercise component

--- src/components/organisms/FormExercise.tsx
+++ src/components/organisms/FormExercise.tsx
@@ -4,16 +4,25 @@ import {zodResolver} from "@hookform/resolvers/zod";
 import {useForm} from "react-hook-form";
 import {z} from "zod";
 
-import {Button} from "@/components/ui/button";
 import {Form} from "@/components/ui/form";
 
 import {toast} from "@/components/ui/use-toast";
 import FormFieldUser from "../molecules/FormFieldUser";
+import FormFieldInputNumber from "../molecules/FormFieldInputNumber";
+import ButtonForm from "../atoms/Buttons/ButtonForm/ButtonForm";
+import {formErrors, inputNumberZ} from "@/lib/constants";
 
 const FormSchema = z.object({
     user: z.string({
-        required_error: "Por favor, selecciona a un usuario.",
+        required_error: formErrors.user,
     }),
+    armLeft: inputNumberZ,
+    armRight: inputNumberZ,
+    legLeft: inputNumberZ,
+    legRight: inputNumberZ,
+    chest: inputNumberZ,
+    weight: inputNumberZ,
+    bfi: inputNumberZ,
 });
 
 const FormExercise = () => {
@@ -38,10 +47,64 @@ const FormExercise = () => {
         <Form {...form}>
             <form
                 onSubmit={form.handleSubmit(onSubmit)}
-                className="w-2/3 space-y-6"
+                className="flex flex-col gap-8 px-4 pt-14"
             >
-                <FormFieldUser form={form} name="user" />
-                <Button type="submit">Submit</Button>
+                <div className="flex flex-col gap-4">
+                    <FormFieldUser form={form} name="user" />
+                    <div className="flex gap-4">
+                        <FormFieldInputNumber
+                            form={form}
+                            placeholder="Brazo Izq. (cm)"
+                            type="number"
+                            name="armLeft"
+                        />
+                        <FormFieldInputNumber
+                            form={form}
+                            placeholder="Brazo Der. (cm)"
+                            type="number"
+                            name="armRight"
+                        />
+                    </div>
+                    <div className="flex gap-4">
+                        <FormFieldInputNumber
+                            form={form}
+                            placeholder="Pierna Izq. (cm)"
+                            type="number"
+                            name="legLeft"
+                        />
+                        <FormFieldInputNumber
+                            form={form}
+                            placeholder="Pierna Der. (cm)"
+                            type="number"
+                            name="legRight"
+                        />
+                    </div>
+
+                    <FormFieldInputNumber
+                        form={form}
+                        placeholder="Pecho (cm)"
+                        type="number"
+                        name="chest"
+                    />
+                    <FormFieldInputNumber
+                        form={form}
+                        placeholder="Peso (kg)"
+                        type="number"
+                        name="weight"
+                    />
+                    <FormFieldInputNumber
+                        form={form}
+                        placeholder="Índice de grasa corporal (%)"
+                        type="number"
+                        name="bfi"
+                    />
+                </div>
+
+                <ButtonForm
+                    type="submit"
+                    onClick={() => {}}
+                    title="Subir ejercicio"
+                />
             </form>
         </Form>
     );
