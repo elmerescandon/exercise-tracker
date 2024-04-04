9435fc7cc137970562a7b50d4cfba9fa6efade79
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-04-03 21:56:45 -0500

    Add FormFieldUser component

---
+++ src/components/molecules/FormFieldUser.tsx
@@ -0,0 +1,30 @@
+import {FormField, FormItem, FormMessage} from "../ui/form";
+import InputSelect from "../atoms/Inputs/InputSelect/InputSelect";
+import {users} from "@/lib/constants";
+
+type FormFieldUserProps = {
+    form: any;
+    name: string;
+};
+
+const FormFieldUser = ({form, name}: FormFieldUserProps) => {
+    return (
+        <FormField
+            control={form.control}
+            name={name}
+            render={({field}) => (
+                <FormItem>
+                    <InputSelect
+                        placeholder="Selecciona al usuario"
+                        defaultValue={field.value}
+                        onChange={field.onChange}
+                        options={users}
+                    />
+                    <FormMessage />
+                </FormItem>
+            )}
+        />
+    );
+};
+
+export default FormFieldUser;
