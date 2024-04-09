7e3a476a8787ef426f60b1b890782829ac703861
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-04-09 11:40:56 -0500

    Add IExerciseLog interface to src/lib/interfaces.ts

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -2,3 +2,14 @@ export interface ISelectOption {
     value: string;
     label: string;
 }
+
+export interface IExerciseLog {
+    user: string;
+    armLeft: number;
+    armRight: number;
+    legLeft: number;
+    legRight: number;
+    chest: number;
+    weight: number;
+    bfi: number;
+}
