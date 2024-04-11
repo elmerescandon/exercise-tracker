f5b94d0d24aec25e3672036ac80b6aa7489d54af
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-04-11 16:26:50 -0500

    Refactor user values to strings in constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,14 +2,21 @@ import type {ISelectOption} from "./interfaces";
 import {date, z} from "zod";
 
 export const users: ISelectOption[] = [
-    {label: "Raúl", value: 1},
-    {label: "Guti", value: 2},
-    {label: "Mapu", value: 3},
-    {label: "Math", value: 4},
+    {label: "Raúl", value: "1"},
+    {label: "Guti", value: "2"},
+    {label: "Mapu", value: "3"},
+    {label: "Math", value: "4"},
 ];
 
+export const userObjects = {
+    1: "Raúl",
+    2: "Guti",
+    3: "Mapu",
+    4: "Math",
+};
+
 export const formErrors = {
-    userName: " El usuario es requerido",
+    userId: " El usuario es requerido",
     metric: "La métrica es requerida",
     date: "La fecha es requerida",
 };
