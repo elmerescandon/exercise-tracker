355dff19ab31cf8cf0c8a31c0b0166a442e36940
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-04-11 13:04:53 -0500

    :sparkles: Insert seed data into the database

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,55 @@
-import { db } from 'astro:db';
+import {ExerciseLog, db} from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+    await db.insert(ExerciseLog).values([
+        {
+            id: 1,
+            date: new Date(),
+            userName: "mathi",
+            armLeft: 27,
+            armRight: 26,
+            legLeft: 46.5,
+            legRight: 47,
+            chest: 90.5,
+            weight: 55,
+            bfi: 16.7,
+        },
+        {
+            id: 2,
+            date: new Date(),
+            userName: "raul",
+            armLeft: 29,
+            armRight: 29,
+            legLeft: 47,
+            legRight: 47,
+            chest: 96,
+            weight: 58,
+            bfi: 17.4,
+        },
+        {
+            id: 3,
+            date: new Date(),
+            userName: "mathi",
+            armLeft: 27.1,
+            armRight: 26.5,
+            legLeft: 46.8,
+            legRight: 46.5,
+            chest: 90,
+            weight: 55,
+            bfi: 16.4,
+        },
+        {
+            id: 4,
+            date: new Date(),
+            userName: "raul",
+            armLeft: 29.3,
+            armRight: 28.2,
+            legLeft: 45.5,
+            legRight: 45,
+            chest: 96.2,
+            weight: 58,
+            bfi: 17.1,
+        },
+    ]);
 }
