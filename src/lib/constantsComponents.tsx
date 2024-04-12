c89a8f0e2b3dea01208c6ed723b04a63174ecc8d
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-04-12 12:52:50 -0500

    Add constantsComponents.tsx file

---
+++ src/lib/constantsComponents.tsx
@@ -0,0 +1,20 @@
+import type {z} from "astro:content";
+import type {FormSchema} from "./constants";
+
+export const toastAddExercise = (data: z.infer<typeof FormSchema>) => {
+    return {
+        title: "You submitted the following values:",
+        description: (
+            <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+                <code className="text-white">
+                    {JSON.stringify(data, null, 2)}
+                </code>
+            </pre>
+        ),
+    };
+};
+
+export const toastExistingExercise = {
+    title: "Ya existe un ejercicio para esta semana",
+    description: "No se ha podido subir el ejercicio",
+};
