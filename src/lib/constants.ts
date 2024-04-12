f066eeb859a98b5f58fd1d54189edfbb0492f40a
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-04-12 12:52:46 -0500

    Refactor constants.ts to add FormSchema and update users array

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,20 +1,6 @@
 import type {ISelectOption} from "./interfaces";
 import {date, z} from "zod";
 
-export const users: ISelectOption[] = [
-    {label: "Raúl", value: "1"},
-    {label: "Guti", value: "2"},
-    {label: "Mapu", value: "3"},
-    {label: "Math", value: "4"},
-];
-
-export const userObjects = {
-    1: "Raúl",
-    2: "Guti",
-    3: "Mapu",
-    4: "Math",
-};
-
 export const formErrors = {
     userId: " El usuario es requerido",
     metric: "La métrica es requerida",
@@ -29,3 +15,33 @@ export const inputNumberZ = z.coerce
         message: formErrors.metric,
     })
     .default(0);
+
+export const FormSchema = z.object({
+    userId: z.string({
+        required_error: formErrors.userId,
+    }),
+    date: z.date({
+        required_error: formErrors.date,
+    }),
+    armLeft: inputNumberZ,
+    armRight: inputNumberZ,
+    legLeft: inputNumberZ,
+    legRight: inputNumberZ,
+    chest: inputNumberZ,
+    weight: inputNumberZ,
+    bfi: inputNumberZ,
+});
+
+export const users: ISelectOption[] = [
+    {label: "Raúl", value: "1"},
+    {label: "Guti", value: "2"},
+    {label: "Mapu", value: "3"},
+    {label: "Math", value: "4"},
+];
+
+export const userObjects = {
+    1: "Raúl",
+    2: "Guti",
+    3: "Mapu",
+    4: "Math",
+};
