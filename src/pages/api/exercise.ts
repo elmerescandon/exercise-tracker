befd3022ae3a28428d1aa9014681f9a07619176f
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-04-12 13:00:48 -0500

    Refactor ExerciseService to use eq function from astro:db in PUT API endpoint

--- src/pages/api/exercise.ts
+++ src/pages/api/exercise.ts
@@ -1,6 +1,6 @@
 import type {IExerciseLog} from "@/lib/interfaces";
 import type {APIContext, APIRoute} from "astro";
-import {db, ExerciseLog} from "astro:db";
+import {db, eq, ExerciseLog} from "astro:db";
 import {getWeek} from "date-fns";
 
 export async function POST(ctx: APIContext): Promise<Response> {
@@ -30,6 +30,49 @@ export async function POST(ctx: APIContext): Promise<Response> {
         return new Response(
             JSON.stringify({
                 message: "Exercise log inserted successfully",
+                exerciseLog: result,
+            }),
+            {status: 200}
+        );
+    } catch (error) {
+        return new Response(
+            JSON.stringify({
+                error: (error as Error).message,
+            }),
+            {status: 500}
+        );
+    }
+}
+
+export async function PUT(ctx: APIContext): Promise<Response> {
+    try {
+        const rawData = await new Response(ctx.request.body).text();
+        const exerciseLog: IExerciseLog = JSON.parse(rawData);
+        if (!exerciseLog) {
+            throw new Error("No request body provided");
+        }
+
+        const newUserId =
+            typeof exerciseLog.userId === "number"
+                ? exerciseLog.userId
+                : parseInt(exerciseLog.userId);
+        const result = await db
+            .update(ExerciseLog)
+            .set({
+                date: new Date(exerciseLog.date),
+                week: getWeek(new Date(exerciseLog.date)),
+                armLeft: exerciseLog.armLeft,
+                armRight: exerciseLog.armRight,
+                legLeft: exerciseLog.legLeft,
+                legRight: exerciseLog.legRight,
+                chest: exerciseLog.chest,
+                weight: exerciseLog.weight,
+                bfi: exerciseLog.bfi,
+            })
+            .where(eq(ExerciseLog.userId, newUserId));
+        return new Response(
+            JSON.stringify({
+                message: "Exercise log updated successfully",
                 exerciseLog: result,
             }),
             {status: 200}
