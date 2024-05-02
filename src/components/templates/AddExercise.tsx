04e60bf2c425b250a6242c87cf5edfa06e310352
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-05-01 21:45:27 -0500

    :sparkles: Add AddExercise template component

---
+++ src/components/templates/AddExercise.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import FormExercise from "../organisms/FormExercise";
+import AlertWrapper from "./AlertWrapper";
+
+const AddExercise = () => {
+    return (
+        <div>
+            <FormExercise />
+        </div>
+    );
+};
+
+export default AddExercise;
