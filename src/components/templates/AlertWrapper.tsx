401770d05c5ff3f09a900ec792751b61d6db4b20
Elmer Raúl Escandón Tufino <elmer.escandon@utec.edu.pe>
2024-05-01 21:45:34 -0500

    Refactor AlertWrapper component to use client instead of React import

--- src/components/templates/AlertWrapper.tsx
+++ src/components/templates/AlertWrapper.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+"use client";
 import {
     AlertDialog,
     AlertDialogAction,
@@ -8,25 +8,27 @@ import {
     AlertDialogFooter,
     AlertDialogHeader,
     AlertDialogTitle,
-    AlertDialogTrigger,
 } from "../ui/alert-dialog";
 
 type AlertWrapperProps = {
-    children: React.ReactNode;
-    onConfirm: () => void;
-    title?: string;
-    description?: string;
+    title: string;
+    description: string;
+    open: boolean;
+    setOpen: (open: boolean) => void;
+    handleAction?: () => void;
+    handleCancel?: () => void;
 };
 
 const AlertWrapper = ({
-    children,
-    onConfirm,
     title,
     description,
+    open,
+    setOpen,
+    handleAction,
+    handleCancel,
 }: AlertWrapperProps) => {
     return (
-        <AlertDialog>
-            <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+        <AlertDialog open={open} onOpenChange={setOpen}>
             <AlertDialogContent>
                 <AlertDialogHeader>
                     <AlertDialogTitle>{title}</AlertDialogTitle>
@@ -35,9 +37,15 @@ const AlertWrapper = ({
                     </AlertDialogDescription>
                 </AlertDialogHeader>
                 <AlertDialogFooter>
-                    <AlertDialogCancel>Cancelar</AlertDialogCancel>
-                    <AlertDialogAction onClick={onConfirm}>
-                        Continuar
+                    <AlertDialogCancel asChild>
+                        <button onClick={handleCancel}>
+                            <span>Cancelar</span>
+                        </button>
+                    </AlertDialogCancel>
+                    <AlertDialogAction asChild>
+                        <button onClick={handleAction}>
+                            <span>Eliminar</span>
+                        </button>
                     </AlertDialogAction>
                 </AlertDialogFooter>
             </AlertDialogContent>
